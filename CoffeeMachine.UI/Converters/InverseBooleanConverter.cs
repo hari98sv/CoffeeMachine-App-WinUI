93df89279854bf1c219e1d78d38640ef3cafd8db
harisankarv98@gmail.com <harisankarv98@gmail.com>
2025-09-03 16:18:37 +0530

    Added basic styling in UI

---
+++ CoffeeMachine.UI/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace CoffeeMachine.UI.Converters;
+
+public class InverseBooleanConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return value is bool boolValue ? !boolValue : value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        return value is bool boolValue ? !boolValue : value;
+    }
+}
